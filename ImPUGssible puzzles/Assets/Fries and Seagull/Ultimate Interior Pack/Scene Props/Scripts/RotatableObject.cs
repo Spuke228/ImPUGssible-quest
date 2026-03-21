2e29491369dfc45ee18ddd2614647a418d67bf04
Spuke228 <tm3419484@gmail.com>
2026-03-21 11:17:30 +0400

    materials

--- ImPUGssible puzzles/Assets/Fries and Seagull/Ultimate Interior Pack/Scene Props/Scripts/RotatableObject.cs
+++ ImPUGssible puzzles/Assets/Fries and Seagull/Ultimate Interior Pack/Scene Props/Scripts/RotatableObject.cs
@@ -1,13 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
-using Seagull.Interior_04E.Inspector;
+using Seagull.Interior_I1.Inspector;
 
 # if UNITY_EDITOR
 # endif
 
 using UnityEngine;
 
-namespace Seagull.Interior_04E.SceneProps {
+namespace Seagull.Interior_I1.SceneProps {
     [Serializable]
     public class String2Rotatable : KiiValuePair<string, Rotatable> {}
     
